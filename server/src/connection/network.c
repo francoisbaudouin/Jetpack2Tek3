4a8f1769a4f5d6b0817600ce79742f7dec59d1b9
François <francois.baudouin@epitech.eu>
2022-07-15 09:31:15 +0200

    add server checkup condition

--- server/src/connection/network.c
+++ server/src/connection/network.c
@@ -7,6 +7,20 @@
 
 #include "server.h"
 
+static bool checkup_configuration(server_t *server)
+{
+    if (server->socket_fd_server == -1) {
+        printf("socket connexion failed\n");
+        close(server->socket_fd_server);
+        return (false);
+    }
+    if (bind(server->socket_fd_server, (struct sockaddr *) &server->socket_addr, sizeof(server->socket_addr)) != 0) {
+        printf("Error bind");
+        close(server->socket_fd_server);
+        return (false);
+    }
+}
+
 int network_configuration(char *port, server_t *server)
 {
     server->socket_fd_server = socket(AF_INET, SOCK_STREAM,
@@ -14,16 +28,8 @@ int network_configuration(char *port, server_t *server)
     server->socket_addr.sin_family = AF_INET;
     server->socket_addr.sin_port = htons(atoi(port));
     server->socket_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (server->socket_fd_server == -1) {
-        printf("socket connexion failed\n");
-        close(server->socket_fd_server);
-        return (-1);
-    }
-    if (bind(server->socket_fd_server, (struct sockaddr *) &server->socket_addr, sizeof(server->socket_addr)) != 0) {
-        printf("Error bind");
-        close(server->socket_fd_server);
-        return (-1);
-    }
+    if (checkup_configuration(server) == false)
+        return (false);
     server->socket_size = sizeof(struct sockaddr);
     bzero(&server->socket_addr, sizeof(server->socket_addr));
     FD_SET(server->socket_fd_server, &server->rfds);
