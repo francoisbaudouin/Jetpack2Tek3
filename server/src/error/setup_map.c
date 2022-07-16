9bbeb1fbec09c0df0af900ca2abad3c8ec001545
François <francois.baudouin@epitech.eu>
2022-07-16 10:26:15 +0200

    add store map

--- server/src/error/setup_map.c
+++ server/src/error/setup_map.c
@@ -13,14 +13,18 @@
 #include <sys/resource.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 
 #define COIN 'c'
 #define ELECTRIC_FENCE 'e'
 #define EMPTY_SPACE ' '
 
-static void store_map(server_t *server, char *line, size_t len)
+static bool store_map(server_t *server, char *line, size_t len, int i)
 {
-    
+    server->map->map[i] = strdup(line);
+    if (server->map->map[i] == NULL)
+        return (true);
+    return (false);
 }
 
 static bool is_good_character(char c)
@@ -34,15 +38,18 @@ static bool is_good_character(char c)
     return (true);
 }
 
-static void load_file_in_mem(server_t *server, char const *filepath)
+static bool load_file_in_mem(server_t *server, char const *filepath)
 {
     FILE *fp = fopen(filepath, "r");
     size_t len = 0;
     char *line;
-
+    int i = 0;
     while (getline(&line, &len, fp) != -1) {
-
+        if (store_map(server, line, len, i) == false)
+            return (false);
+        i++;
     }
+    (void) len;
     fclose(fp);
     free(line);
 }
@@ -52,6 +59,7 @@ bool load_map(server_t *server, char **argv)
     if (strcmp(argv[6], "-m") != 0)
         return (false);
     server->map = malloc(sizeof(map_t));
+    load_file_in_mem(server, argv[7]);
     if (server->map == NULL)
         return (false);
     return (true);
